8496e07295d7ee59f62f05d9542210b9c270aa6e
Grishchenko Daniil <grishchenko_daniil_1@mail.ru>
2022-06-28 21:08:53 +0300

    Lesson 24_Ruby_sugar/homework1-done

--- Lesson 24_Ruby_sugar/homework1/main.rb
+++ Lesson 24_Ruby_sugar/homework1/main.rb
@@ -6,6 +6,6 @@ if output_count.nil?
 end
 #Пришлось выделить отдельную строку, так как иначе пришлось бы в двух местах писать .to_i на стр: 9, 11
 output_count = output_count.to_i
-output_count = 10 if output_count == 0
+output_count = 10 if output_count == 0 || output_count.negative?
 
 output_count.times { puts Time.now }
