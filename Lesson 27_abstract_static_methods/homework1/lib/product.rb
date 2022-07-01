9669dc697a3650c7515d40527f0c8f3372b9d622
Grishchenko Daniil <grishchenko_daniil_1@mail.ru>
2022-07-02 00:04:30 +0300

    Lesson 27_abstract_static_methods/homework1-done

--- Lesson 27_abstract_static_methods/homework1/lib/product.rb
+++ Lesson 27_abstract_static_methods/homework1/lib/product.rb
@@ -5,14 +5,6 @@ class Product
     @amount_on_warehouse = params[:amount_on_warehouse]
   end
 
-  def self.get_type
-    [Book, Film]
-  end
-
-  def self.create(type_num, params)
-    get_type[type_num].new(params)
-  end
-
   def to_s
     #abstract
   end
--- Lesson 27_abstract_static_methods/homework1/main.rb
+++ Lesson 27_abstract_static_methods/homework1/main.rb
@@ -4,10 +4,10 @@ require_relative "lib/film"
 
 products = []
 # types: 0 - book, 1 - film
-products << Product.create(0, {name: "Count of Monte Cristo", genre: "novel",
-                                             author: "Duma", price: 14, amount_on_warehouse: 7})
+products << Book.new({name: "Count of Monte Cristo", genre: "novel",
+                             author: "Duma", price: 14, amount_on_warehouse: 7})
 
-products << Product.create(1, {name: "Space odyssey", director: "Cubrick",
-                                             price: 20, amount_on_warehouse:3, year: 1964})
+products << Film.new({name: "Space odyssey", director: "Cubrick",
+                             price: 20, amount_on_warehouse:3, year: 1964})
 puts products[0].to_s
 puts products[1].to_s
