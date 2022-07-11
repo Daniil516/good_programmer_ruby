877d28c9479268eca0917e01fb9de59017d9e518
Grishchenko Daniil <grishchenko_daniil_1@mail.ru>
2022-07-11 13:26:17 +0300

    update

--- Lesson 31_Rspec/homework3/spec/film_spec.rb
+++ Lesson 31_Rspec/homework3/spec/film_spec.rb
@@ -6,19 +6,19 @@ describe Film do
 
   describe "#new" do
     it 'should be a Film instance' do
-      film.should be_an_instance_of Film
+      expect(film).to be_an_instance_of Film
     end
 
     it 'should return correct film_name' do
-      film.film_name.should eql "Back to the future"
+      expect(film.film_name).to eql "Back to the future"
     end
 
     it 'should return correct film_director' do
-      film.director.should eql "Robert Zemeckis"
+      expect(film.director).to eql "Robert Zemeckis"
     end
 
     it 'should return correct year' do
-      film.year.should eql 1985
+      expect(film.year).to eql 1985
     end
   end
 end
--- Lesson 32_Json/homework4/main.rb
+++ Lesson 32_Json/homework4/main.rb
@@ -1,16 +1,16 @@
 require "json"
 
-file = File.read("the_periodic_table.json")
-table = JSON.parse(file)
+json_string = File.read("#{__dir__}/the_periodic_table.json")
+table = JSON.parse(json_string, symbolize_names: true)
 
 # Printing amount of elements
-puts "We have #{table.keys.size} elements"
+puts "We have #{table.size} elements"
 
 # Printing elements
 puts table.keys.join(", ")
 
 puts "What element do you want to know about?"
-element = gets.chomp
+element = $stdin.gets.chomp.downcase.capitalize.to_sym
 
 # Check if program has element
 unless table.key?(element)
@@ -20,8 +20,8 @@ end
 
 hash_info = table[element]
 puts <<~ELEMENT_INFO
-Table number: #{hash_info["table_number"]}
-Name: #{hash_info["name"]}
-Discoverer: #{hash_info["discoverer"]} (#{hash_info["year"]} y.)
-Density: #{hash_info["density"]} g/cm³
+  Table number: #{hash_info[:table_number]}
+  Name: #{hash_info[:name]}
+  Discoverer: #{hash_info[:discoverer]} (#{hash_info[:year]} y.)
+  Density: #{hash_info[:density]} g/cm³
 ELEMENT_INFO
