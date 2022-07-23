c9798e2ad18f95f8ec3fad6297554c47c712dc1d
Daniil Grishchenko <grishchenko_daniil_1@mail.ru>
2022-07-23 13:34:33 +0300

    lesson41-homework1-update

--- Field/testfield.rb
+++ Field/testfield.rb
@@ -1,13 +1,1 @@
-require 'nokogiri'
-require 'open-uri'
-
-# Fetch and parse HTML document
-uri = URI.open("https://www.imdb.com/list/ls055592025/")
-doc = Nokogiri::HTML(uri)
-
-# names
-film_names = doc.css(".lister-item-content h3 a").map { |name| name.text  }
-pp film_names
-
-
-
+puts (1..3).inject {|i, x| i + x}
